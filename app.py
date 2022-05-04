3c69e64bac7d7627d3038c54f39e32bd600ed975
Nirodya Pussadeniya <en95142@sjp.ac.lk>
2022-05-04 23:57:00 +0530

    updated

---
+++ app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+st.set_page_config(layout="wide")
+
+
+from zoom import ZOOM_CLIENT
+
+
+z = ZOOM_CLIENT()
+
+st.title("Virtual KYC on Zoom - POC")
+
+meetings = z.list_meetings()
+
+if meetings['meetings']:
+    st.write("Scheduled Meetings")
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.header("Topic")
+    with col2:
+        st.header("Start Time")
+    with col3:
+        st.header("Join Link")
+
+    for meeting in meetings['meetings']:
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            st.write(meeting['topic'])
+
+        with col2:
+            _date, _time = meeting['start_time'].split("T")
+            st.write(_date + " " + _time[:5])
+
+        with col3:
+            st.write(meeting['join_url'])
+            # st.button("JOIN", key=meeting['uuid'], on_click=meeting['join_url'])
+
+else:
+    with st.form("Meeting Information", clear_on_submit=True):
+        topic = st.text_input("Meeting Topic")
+        agenda = st.text_input("Detailed Meeting Agenda")
+
+        col1, col2 = st.columns(2)
+        with col1:
+            date = st.date_input("Start Date")
+        with col2:
+            time = st.time_input("Start Time")
+        
+        invitees = st.text_input("Meeting Invitees", placeholder="Comma separated list of meeting invitees")
+        upload = st.form_submit_button("Schedule meeting")
+
+        if upload:
+            start_time = str(date)+"T"+str(time)
+            z.create_meeting(topic, agenda, start_time, invitees)
+            st.experimental_rerun()
---
+++ zoom.py
@@ -0,0 +1,73 @@
+import jwt
+from time import time
+import json
+import requests
+
+# JWT_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6IjZtazllYmpmVEJxUUhRd01Xb05UMFEiLCJleHAiOjE2NTIyODA3OTEsImlhdCI6MTY1MTY3NTk5Mn0.r6_sUlQF974zVMSOU_2JahgMjCaow9WoQog12ZDNOQA'
+# Verification_Token = '2zQMPtltR6SqhJavHaFvzA'
+
+
+class ZOOM_CLIENT():
+
+    def __init__(self) -> None:
+        self.API_KEY = 'pnnUcpQSS4-aV1pPrFi9Ng'
+        self.API_SECRET = 'hnZYNbmowkIs039SJE3MKx6umc4p29YxbA6H'
+        self.BASE_URL = 'https://api.zoom.us/v2/'
+        self.headers = {
+            'authorization': 'Bearer ' + self.__generate_token(),
+            'content-type': 'application/json'
+        }
+
+    def __generate_token(self):
+        token = jwt.encode(
+            # API Key & expiration time
+            {'iss': self.API_KEY, 'exp': time() + 5000},
+            # Secret used to generate token signature
+            self.API_SECRET,
+            # Specify the hashing alg
+            algorithm='HS256'
+        )
+        return token
+
+    def create_meeting(self, topic, agenda, start_time, invitees):
+        _invitees, _emails = [], invitees.split(";")
+        for _email in _emails:
+            _invitees.append({
+                "email": _email
+            })
+
+        payload = {
+            "agenda": agenda,
+            "duration": 30,
+            "password": "123456",
+            "schedule_for": "shayeny@kainovation.com",
+            "settings": {
+                "approval_type": 2,
+                "audio": "both",
+                "auto_recording": "cloud",
+                "calendar_type": 2,
+                "contact_email": "shayeny@kainovation.com",
+                "contact_name": "Shayen Yatagama",
+                "email_notification": True,
+                "focus_mode": True,
+                "host_video": False,
+                "jbh_time": 5,
+                "join_before_host": False,
+                "meeting_authentication": False,
+                "meeting_invitees": _invitees,
+                "mute_upon_entry": True,
+                "participant_video": True,
+            },
+            "start_time":  start_time,
+            "timezone": "Asia/Colombo",
+            "topic": topic,
+            "type": 2
+        }
+
+        return requests.post(self.BASE_URL + 'users/me/meetings', headers=self.headers, data=json.dumps(payload)
+                             )
+
+    def list_meetings(self):
+        meetings = requests.get(
+            self.BASE_URL + 'users/me/meetings', headers=self.headers)
+        return json.loads(meetings.text)
